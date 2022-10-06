4329f15333f16f8f9725b2ca370f603355c572af
cchaae <kkang_chung125@yahoo.co.jp>
2022-10-06 11:48:17 +0900

    feat: frontend側作成

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,8 +1,7 @@
-// Entry point for the build script in your package.json
-import "@hotwired/turbo-rails"
-import "./controllers"
+/* eslint-disable no-undef */
 import React, { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
 import App from './components/App';
 
 const container = document.getElementById('root');
@@ -11,7 +10,9 @@ const root = createRoot(container);
 document.addEventListener('DOMContentLoaded', () => {
   root.render(
     <StrictMode>
-      <App />
-    </StrictMode>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </StrictMode>,
   );
 });

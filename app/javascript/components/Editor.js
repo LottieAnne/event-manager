45c4063c8d71e8c86cf4e8407b847607d21f7ca7
cchaae <kkang_chung125@yahoo.co.jp>
2022-10-06 13:12:15 +0900

    feat: Eventの作成

--- app/javascript/components/Editor.js
+++ app/javascript/components/Editor.js
@@ -3,6 +3,7 @@ import { Routes, Route } from 'react-router-dom';
 import Header from './Header';
 import EventList from './EventList';
 import Event from './Event';
+import EventForm from './EventForm';
 
 const Editor = () => {
   const [events, setEvents] = useState([]);
@@ -30,13 +31,13 @@ const Editor = () => {
   return (
     <>
       <Header />
-      <div className='grid'>
-        isError && <p>Something went wrong. Check the console.</p>}
+      <div className="grid">
         {isLoading ? (<p>Loading...</p>) : (
           <>
             <EventList events={events} />
 
             <Routes>
+              <Route path="new" element={<EventForm />} />
               <Route path=":id" element={<Event events={events} />} />
             </Routes>
           </>
